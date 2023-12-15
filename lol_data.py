13018b96011b9732a04c33d319d7daaa4ea1647d
Joram Swarts <152791@student.horizoncollege.nl>
2023-12-15 12:34:17 +0100

    saving testing space

--- lol_data.py
+++ lol_data.py
@@ -12,3 +12,150 @@ def get_summoner_lol_data(api_key, summoner_name, region):
     else:
         print(f"Error: {response.status_code}, {response.text}")
         return None
+
+
+def get_match_ids(api_key, puuid, region, queue_type=None, start_time=None, end_time=None, count=50):
+    region_mappings = {
+        'na1': 'americas',
+        'br1': 'americas',
+        'lan': 'americas',
+        'las': 'americas',
+        'kr': 'asia',
+        'jp': 'asia',
+        'eune': 'europe',
+        'euw1': 'europe',
+        'tr1': 'europe',
+        'ru': 'europe',
+        'oce': 'sea',
+        'ph2': 'sea',
+        'sg2': 'sea',
+        'th2': 'sea',
+        'tw2': 'sea',
+        'vn2': 'sea',
+    }
+
+    queue_type_mappings = {
+        'ranked_solo_duo': 420,
+        'ranked_flex': 440,
+        'blind_pick': 430,
+        'draft_pick': 400,
+        'clash': 700
+    }
+
+    region = region.lower()
+    routing_value = region_mappings.get(region, region)
+
+    # Convert user-friendly queue type to numeric queue ID
+    queue_id = queue_type_mappings.get(queue_type.lower())
+
+    if queue_id is None:
+        print("Invalid queue type. Please enter a valid queue type: ranked_solo_duo, ranked_flex, blind_pick, draft_pick, clash")
+        return None
+
+    matchlist_url = f'https://{routing_value}.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids'
+    headers = {'X-Riot-Token': api_key}
+
+    params = {'startTime': start_time, 'endTime': end_time, 'count': count, 'queue': queue_id}
+
+    try:
+        response = requests.get(matchlist_url, headers=headers, params=params)
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(f"Error fetching match IDs: {response.status_code}, {response.text}")
+            return None
+    except Exception as e:
+        print(f"Exception during match ID retrieval: {e}")
+        return None
+    
+
+def get_match_data(api_key, match_id, region):
+    routing_value = get_routing_value(region)
+
+    match_url = f'https://{routing_value}.api.riotgames.com/lol/match/v5/matches/{match_id}'
+
+    headers = {'X-Riot-Token': api_key}
+
+    try:
+        response = requests.get(match_url, headers=headers)
+
+        if response.status_code == 200:
+            match_data = response.json()
+            # Voeg hier een controle toe voor het gametype
+            if 'info' in match_data and 'gameMode' in match_data['info'] and match_data['info']['gameMode'].lower() == 'classic':
+                return match_data
+            else:
+                print(f"Skipping match ID {match_id} as it is not a classic game.")
+                return None
+        else:
+            print(f"Error fetching match data for match ID {match_id}: {response.status_code}, {response.text}")
+            return None
+    except Exception as e:
+        print(f"Exception during match data retrieval: {e}")
+        return None
+    
+def calculate_win_percentage_per_role(matches, summoner_puuid):
+    total_matches = len(matches)
+
+    # Filter out None values from matches
+    valid_matches = [match for match in matches if match is not None]
+
+    # Check if there are valid matches to calculate win percentage
+    if valid_matches:
+        overall_wins = 0
+        overall_matches_with_wins = 0
+
+        role_wins = {role: 0 for role in ["TOP", "JUNGLE", "MIDDLE", "BOTTOM", "UTILITY"]}
+        role_matches_with_wins = {role: 0 for role in ["TOP", "JUNGLE", "MIDDLE", "BOTTOM", "UTILITY"]}
+
+        for match in valid_matches:
+            if 'info' in match and 'participants' in match['info']:
+                for participant in match['info']['participants']:
+                    if 'individualPosition' in participant:
+                        role = participant['individualPosition']
+                        # Check if the role is valid
+                        if role in role_wins:
+                            # Check if the summoner is the participant and if they won
+                            if participant['puuid'] == summoner_puuid:
+                                overall_matches_with_wins += 1
+                                role_matches_with_wins[role] += 1
+                                if participant.get('win', False):
+                                    overall_wins += 1
+                                    role_wins[role] += 1
+
+        overall_win_percentage = (overall_wins / overall_matches_with_wins) * 100 if overall_matches_with_wins > 0 else 0
+        print(f"Overall Win Percentage: {overall_win_percentage:.2f}%")
+
+        win_percentages = {"Overall": overall_win_percentage}
+
+        for role in ["TOP", "JUNGLE", "MIDDLE", "BOTTOM", "UTILITY"]:
+            role_win_percentage = (role_wins[role] / role_matches_with_wins[role]) * 100 if role_matches_with_wins[role] > 0 else 0
+            print(f"Win Percentage for {role.capitalize()} role: {role_win_percentage:.2f}%")
+            win_percentages[role.capitalize()] = role_win_percentage
+
+        return win_percentages
+    else:
+        print("No valid match data available.")
+
+def get_routing_value(region):
+    region_mappings = {
+        'na1': 'americas',
+        'br1': 'americas',
+        'lan': 'americas',
+        'las': 'americas',
+        'kr': 'asia',
+        'jp': 'asia',
+        'eune': 'europe',
+        'euw1': 'europe',
+        'tr1': 'europe',
+        'ru': 'europe',
+        'oce': 'sea',
+        'ph2': 'sea',
+        'sg2': 'sea',
+        'th2': 'sea',
+        'tw2': 'sea',
+        'vn2': 'sea',
+    }
+
+    return region_mappings.get(region.lower(), region.lower())
